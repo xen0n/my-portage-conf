c803cbf8426f025524fbae0a220f6acb20123bdd
Wang Xuerui <git@xen0n.name>
2018-06-23 16:37:28 +0800

    refactor out cflags

---
+++ flavors/cflags/amd64-base.sh
@@ -0,0 +1,5 @@
+# WARNING: Changing your CHOST is not something that should be done lightly.
+# Please consult http://www.gentoo.org/doc/en/change-chost.xml before changing.
+CHOST="x86_64-pc-linux-gnu"
+
+ACCEPT_KEYWORDS="~amd64"
---
+++ flavors/cflags/snb-gcc-7.3.0.sh
@@ -0,0 +1,7 @@
+source ./amd64-base.sh
+
+
+# this CFLAGS is the same as using -march=native, to prevent possible
+# distributed compiling issue later when I decided to do so...
+CFLAGS="-O2 -pipe -march=sandybridge -mmmx -mno-3dnow -msse -msse2 -msse3 -mssse3 -mno-sse4a -mcx16 -msahf -mno-movbe -maes -mno-sha -mpclmul -mpopcnt -mno-abm -mno-lwp -mno-fma -mno-fma4 -mno-xop -mno-bmi -mno-sgx -mno-bmi2 -mno-tbm -mavx -mno-avx2 -msse4.2 -msse4.1 -mno-lzcnt -mno-rtm -mno-hle -mno-rdrnd -mno-f16c -mno-fsgsbase -mno-rdseed -mno-prfchw -mno-adx -mfxsr -mxsave -mxsaveopt -mno-avx512f -mno-avx512er -mno-avx512cd -mno-avx512pf -mno-prefetchwt1 -mno-clflushopt -mno-xsavec -mno-xsaves -mno-avx512dq -mno-avx512bw -mno-avx512vl -mno-avx512ifma -mno-avx512vbmi -mno-avx5124fmaps -mno-avx5124vnniw -mno-clwb -mno-mwaitx -mno-clzero -mno-pku -mno-rdpid -mtune=sandybridge"
+CXXFLAGS="${CFLAGS}"
