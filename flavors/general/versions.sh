aa2fed7f7fb63ca9520c4d347fcb5c4856b70fc9
Wang Xuerui <git@xen0n.name>
2018-06-23 17:45:57 +0800

    update versions.sh
    this brings ld50 almost completely equivalent to the config before
    migration; nice maintenance could start!

--- flavors/general/versions.sh
+++ flavors/general/versions.sh
@@ -1,7 +1,7 @@
 # Python targets
-PYTHON_TARGETS="python2_7 python3_5 python3_6"  # pypy
-PYTHON_SINGLE_TARGET="python3_5"
-USE_PYTHON="2.7 3.5 3.6"
+PYTHON_TARGETS="python2_7 python3_6"  # pypy
+PYTHON_SINGLE_TARGET="python3_6"
+USE_PYTHON="2.7 3.6"
 
 # Ruby
-RUBY_TARGETS="ruby24 ruby25"
+RUBY_TARGETS="ruby25"
