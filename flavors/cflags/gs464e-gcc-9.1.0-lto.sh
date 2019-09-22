e723e899b281e4247df211f93717e69506adecdc
Wang Xuerui <git@xen0n.name>
2019-09-22 15:16:55 +0800

    CFLAGS: -g -> -ggdb

--- flavors/cflags/gs464e-gcc-9.1.0-lto.sh
+++ flavors/cflags/gs464e-gcc-9.1.0-lto.sh
@@ -1,6 +1,6 @@
 source ./mips64el-base.sh
 
 
-CFLAGS="-march=loongson3a -mtune=loongson3a -falign-functions=32 ${CFLAGS} -pipe -g"
+CFLAGS="-march=loongson3a -mtune=loongson3a -falign-functions=32 ${CFLAGS} -pipe -ggdb"
 CXXFLAGS="${CFLAGS}"
 #LDFLAGS="${LDFLAGS} -Wl,--hash-style=gnu"
