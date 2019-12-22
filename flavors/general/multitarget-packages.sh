ebca6b576b1ca850ef2bbb689a9785da36a33833
Wang Xuerui <git@xen0n.name>
2019-12-22 15:18:00 +0800

    build more qemu arches (notably riscv)

--- flavors/general/multitarget-packages.sh
+++ flavors/general/multitarget-packages.sh
@@ -2,5 +2,5 @@
 LLVM_TARGETS="AArch64 AMDGPU ARM BPF Mips NVPTX RISCV WebAssembly X86"
 
 # QEMU targets
-QEMU_SOFTMMU_TARGETS="alpha arm aarch64 i386 mips mips64 mips64el mipsel ppc ppc64 ppcemb s390x unicore32 x86_64"
-QEMU_USER_TARGETS="arm aarch64 mips64el"
+QEMU_SOFTMMU_TARGETS="alpha arm aarch64 i386 mips mips64 mips64el mipsel or1k ppc ppc64 riscv32 riscv64 s390x x86_64"
+QEMU_USER_TARGETS="alpha arm aarch64 i386 mips64el or1k riscv32 riscv64 x86_64"
