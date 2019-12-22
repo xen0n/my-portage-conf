5097b7f93667bd56569c5a03722363a4ae76637f
Wang Xuerui <git@xen0n.name>
2019-12-22 20:42:06 +0800

    add python3.7 and python3.8

--- flavors/general/versions.sh
+++ flavors/general/versions.sh
@@ -1,7 +1,7 @@
 # Python targets
-PYTHON_TARGETS="python2_7 python3_6"  # pypy
-PYTHON_SINGLE_TARGET="python3_6"
-USE_PYTHON="2.7 3.6"
+PYTHON_TARGETS="python2_7 python3_6 python3_7 python3_8"  # pypy
+PYTHON_SINGLE_TARGET="python3_7"
+USE_PYTHON="2.7 3.6 3.7 3.8"
 
 # Ruby
 RUBY_TARGETS="ruby26"
