1b44f7b07e4d7005837d331c10b21bf023abc98b
Wang Xuerui <git@xen0n.name>
2020-02-14 12:20:52 +0800

    hareru: sync

--- flavors/cflags/gs464v-gcc-9.1.0-lto.sh
+++ flavors/cflags/gs464v-gcc-9.1.0-lto.sh
@@ -1,6 +1,7 @@
 source ./mips64el-base.sh
 
 
+#CFLAGS="-march=gs464v -mtune=gs464v -falign-functions=32 ${CFLAGS} -pipe -ggdb -mno-unaligned-accesses"
 CFLAGS="-march=gs464e -mtune=gs464e -mmsa -falign-functions=32 ${CFLAGS} -pipe -ggdb"
 CXXFLAGS="${CFLAGS}"
-LDFLAGS="${LDFLAGS} -Wl,--hash-style=gnu"
+#LDFLAGS="${LDFLAGS} -Wl,--hash-style=gnu"
