e44c7816aeaa14315af4fc30f90b6fe5f9c8a656
Wang Xuerui <git@xen0n.name>
2020-02-22 14:58:01 +0800

    use march=mips64r2 for gs464v

--- flavors/cflags/gs464v-gcc-9.1.0-lto.sh
+++ flavors/cflags/gs464v-gcc-9.1.0-lto.sh
@@ -2,6 +2,9 @@ source ./mips64el-base.sh
 
 
 #CFLAGS="-march=gs464v -mtune=gs464v -falign-functions=32 ${CFLAGS} -pipe -ggdb -mno-unaligned-accesses"
-CFLAGS="-march=gs464e -mtune=gs464e -mmsa -falign-functions=32 ${CFLAGS} -pipe -ggdb"
+#CFLAGS="-march=gs464e -mtune=gs464e -mmsa -falign-functions=32 ${CFLAGS} -pipe -ggdb"
+
+# gs464e pessimizes things by generating branch likely insns it seems
+CFLAGS="-march=mips64r2 -mtune=gs464e -mmsa -falign-functions=32 ${CFLAGS} -pipe -ggdb"
 CXXFLAGS="${CFLAGS}"
 #LDFLAGS="${LDFLAGS} -Wl,--hash-style=gnu"
