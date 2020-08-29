921fe9ccfe43076b393ccca5eaf1ef0efb618cb3
WANG Xuerui <git@xen0n.name>
2020-08-30 01:54:18 +0800

    bump global python version to prefer 3.8 only

--- flavors/general/versions.sh
+++ flavors/general/versions.sh
@@ -1,7 +1,7 @@
 # Python targets
-PYTHON_TARGETS="python3_7 python3_8"  # pypy
-PYTHON_SINGLE_TARGET="python3_7"
-USE_PYTHON="3.7 3.8"
+PYTHON_TARGETS="python3_8"  # pypy
+PYTHON_SINGLE_TARGET="python3_8"
+USE_PYTHON="3.8"
 
 # Ruby
 RUBY_TARGETS="ruby27"
