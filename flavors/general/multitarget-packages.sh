2c9ef74d71282f18e85c54edd34078c7898b858c
WANG Xuerui <git@xen0n.name>
2022-02-05 12:05:15 +0800

    sync

--- flavors/general/multitarget-packages.sh
+++ flavors/general/multitarget-packages.sh
@@ -3,4 +3,4 @@ LLVM_TARGETS="AArch64 AMDGPU ARM BPF Mips NVPTX RISCV WebAssembly X86"
 
 # QEMU targets
 QEMU_SOFTMMU_TARGETS="alpha arm aarch64 i386 mips mips64 mips64el mipsel or1k ppc ppc64 riscv32 riscv64 s390x x86_64"
-QEMU_USER_TARGETS="alpha arm aarch64 i386 mips64el or1k riscv32 riscv64 x86_64"
+QEMU_USER_TARGETS="alpha arm aarch64 hppa i386 mips mips64 mips64el mipsel mipsn32 mipsn32el or1k ppc ppc64 ppc64le riscv32 riscv64 x86_64"
