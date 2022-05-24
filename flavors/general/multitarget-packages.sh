72f5f27a8b2da6ad863b2b51870ae355fde92114
WANG Xuerui <git@xen0n.name>
2022-05-24 23:02:55 +0800

    sync after world upgrade

--- flavors/general/multitarget-packages.sh
+++ flavors/general/multitarget-packages.sh
@@ -2,5 +2,5 @@
 LLVM_TARGETS="AArch64 AMDGPU ARM BPF Mips NVPTX RISCV WebAssembly X86"
 
 # QEMU targets
-QEMU_SOFTMMU_TARGETS="alpha arm aarch64 hppa i386 m68k mips mips64 mips64el mipsel or1k ppc ppc64 riscv32 riscv64 s390x x86_64"
-QEMU_USER_TARGETS="alpha arm aarch64 hppa i386 m68k mips mips64 mips64el mipsel mipsn32 mipsn32el or1k ppc ppc64 ppc64le riscv32 riscv64 x86_64"
+QEMU_SOFTMMU_TARGETS="alpha arm aarch64 hppa i386 loongarch64 m68k mips mips64 mips64el mipsel or1k ppc ppc64 riscv32 riscv64 s390x x86_64"
+QEMU_USER_TARGETS="alpha arm aarch64 hppa i386 loongarch64 m68k mips mips64 mips64el mipsel mipsn32 mipsn32el or1k ppc ppc64 ppc64le riscv32 riscv64 x86_64"
