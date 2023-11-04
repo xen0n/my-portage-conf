03fc3d1b3b1055ff2dfd6ea797075124330d27d9
WANG Xuerui <git@xen0n.name>
2023-11-04 14:04:03 +0800

    ld50: absorb node-flavor.sh back into make.conf
    pointless otherwise

--- nodes/ld50/node-flavor.sh
+++
@@ -1,8 +0,0 @@
-INPUT_DEVICES="evdev libinput wacom"
-VIDEO_CARDS="amdgpu radeon radeonsi"
-
-GRUB_PLATFORMS="efi-64"
-
-GPSD_PROTOCOLS="${GPSD_PROTOCOLS} nmea0183 nmea2000"
-
-PYTHON_TARGETS="python3_10 python3_11"
