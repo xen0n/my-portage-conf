0bf07c8657b9b17d7beec955be8f90b0238afb1d
WANG Xuerui <git@xen0n.name>
2024-04-04 14:05:42 +0800

    remove the explicit CFLAGS config
    I haven't customized this for a long long time, so let's just remove it
    as per the 23.0 migration news item.

--- flavors/cflags/amd64-base.sh
+++ flavors/cflags/amd64-base.sh
@@ -1,7 +1,3 @@
-# WARNING: Changing your CHOST is not something that should be done lightly.
-# Please consult http://www.gentoo.org/doc/en/change-chost.xml before changing.
-CHOST="x86_64-pc-linux-gnu"
-
 ACCEPT_KEYWORDS="~amd64"
 
 # ABI
